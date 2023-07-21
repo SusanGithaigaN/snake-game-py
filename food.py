82a00b63d5faab16c810afd1fc1a0c5ab417a97f
Susan Njeri Githaiga <susangithaiga26@gmail.com>
2023-07-21 10:19:30 +0300

    Added food & class inheritance

---
+++ food.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        # stretch turtle along it's length & width
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        # move food to random location
+        random_x = random.randint(-280, -280)
+        random_y = random.randint(-280, -280)
+        self.goto(random_x, random_y)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # move the snake
 # control snake movements
 import time
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
-
+from food import Food
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -11,6 +11,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,7 +23,9 @@ game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
-
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        # print("breakfast is served")
+        food.refresh()
 screen.exitonclick()
